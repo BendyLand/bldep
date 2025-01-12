6b755423ac51afe5b90a6d9554d1d013da76a7e4
BendyLand <128928536+BendyLand@users.noreply.github.com>
2025-01-11 19:11:42 -0500

    Update filter.py

--- filter.py
+++ filter.py
@@ -4,12 +4,8 @@ with open("src/headers.txt") as file:
     for line in file:
         lines.append(line)
 
-lines.sort()
-result = []
-for line in lines:
-    if line not in result:
-        result.append(line)
+result = sorted(list(set(lines)))
 
 with open("src/headers.txt", "w") as file:
-    for line in lines:
+    for line in result:
         file.write(line)
